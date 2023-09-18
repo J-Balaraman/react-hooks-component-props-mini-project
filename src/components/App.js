395a3f63ec03afd75aea1eca437f0a4c4e9bba2c
jbalaraman <james.balaraman@gmail.com>
2023-09-17 22:04:37 -0600

    Added file components, basic necessities for each component

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,11 @@ console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <About  />
+      <Article  />
+      <ArticleList  />
+      <Header  />
     </div>
   );
 }
